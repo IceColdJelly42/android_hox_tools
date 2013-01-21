f2a832f9e6f16d182fe6b57467454f9f42efe331
LorD ClockaN <davor@losinj.com>
2013-01-21 22:59:13 +0100

    build hox_tools only for endeavoru

--- Android.mk
+++ Android.mk
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include $(call all-subdir-makefiles)
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),endeavoru)
+    include $(call all-subdir-makefiles)
+endif
